fccf5e004812e2cdad71537b6ccd4c8134450505
Pham Mai Chi <maichith16603@gmail.com>
2023-11-15 17:15:52 +0700

    classmembers

--- AimsProject/src/AimsProject/DigitalVideoDisc.java
+++ AimsProject/src/AimsProject/DigitalVideoDisc.java
@@ -1,11 +1,19 @@
 package AimsProject;
 
 public class DigitalVideoDisc {
+	// Pham Mai Chi 20214998
+	// Class member
+	private static int nbDigitalVideoDiscs = 0;
+	
+	// Pham Mai Chi 20214998
+	// Instance member
 	private String title;
 	private String category;
 	private String director;
 	private int length;
 	private float cost;
+	
+	private int id = 0;
 	
 	// Pham Mai Chi 20214998
 	// Getter method
@@ -24,26 +32,46 @@ public class DigitalVideoDisc {
 	public float getCost() {
 		return cost;
 	}
-	
+	public int getId() {
+		return id;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	public void setLength(int length) {
+		this.length = length;
+	}
+	public void setCost(float cost) {
+		this.cost = cost;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public static int getNbDigitalVideoDiscs() {
+		return nbDigitalVideoDiscs;
+	}
+	public static void setNbDigitalVideoDiscs(int nbDigitalVideoDiscs) {
+		DigitalVideoDisc.nbDigitalVideoDiscs = nbDigitalVideoDiscs;
+	}
 	// Pham Mai Chi 20214998
 	// Create Constructor method
 	// Create a DVD object by title
-	
-	
 	public DigitalVideoDisc(String title) {
 		super();
 		this.title = title;
 	}
 	
-	public void setTitle(String title) {
-		this.title = title;
-	}
 	// Create a DVD object by category, title and cost
 	public DigitalVideoDisc(String title, String category, float cost) {
 		super();
 		this.title = title;
 		this.category = category;
 		this.cost = cost;
+		nbDigitalVideoDiscs++;
+		this.id=nbDigitalVideoDiscs;
 	}
 	// Create a DVD object by director, category, title and cost
 	public DigitalVideoDisc(String title, String category, String director, float cost) {
@@ -52,6 +80,8 @@ public class DigitalVideoDisc {
 		this.category = category;
 		this.director = director;
 		this.cost = cost;
+		nbDigitalVideoDiscs++;
+		this.id=nbDigitalVideoDiscs;
 	}
 	// Create a DVD object by all attributes
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
@@ -61,5 +91,8 @@ public class DigitalVideoDisc {
 		this.director = director;
 		this.length = length;
 		this.cost = cost;
-	}	
+		nbDigitalVideoDiscs++;
+		this.id=nbDigitalVideoDiscs;
+	}
+	
 }
